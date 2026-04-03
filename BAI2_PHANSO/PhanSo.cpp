63fd3e0ca59d1e056ef62720e9a1a6bee6f6966b
Nhattuisme18 <25521995@gm.uit.edu.vn>
2026-04-03 17:09:21 +0700

    Update PhanSo.cpp

--- BAI2_PHANSO/PhanSo.cpp
+++ BAI2_PHANSO/PhanSo.cpp
@@ -1,75 +1,112 @@
-#include <iostream>
 #include "PhanSo.h"
+#include <iostream>
+#include <cmath>  
+
 using namespace std;
 
-// Hàm tìm UCLN
+PhanSo::PhanSo(int tu, int mau) {
+	iTu = tu;
+	iMau = (mau != 0) ? mau : 1;
+}
+
 int UCLN(int a, int b) {
-    if (b == 0) return a;
-    return UCLN(b, a % b);
+	if (b == 0) return a;
+	return UCLN(b, a % b);
 }
 
 void PhanSo::Nhap() {
-    cout << "Nhap tu: ";
-    cin >> iTu;
-    do {
-        cout << "Nhap mau (khac 0): ";
-        cin >> iMau;
-    } while (iMau == 0);
+	while (true) {
+		cout << "Nhap tu so: ";
+		if (cin >> iTu) break;
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "Tu so phai la so nguyen. Vui long nhap lai!\n";
+	}
+
+	while (true) {
+		cout << "Nhap mau so (khac 0): ";
+		if (cin >> iMau && iMau != 0) break;
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "Mau so phai la so nguyen. Vui long nhap lai!\n";
+		}
+		else if (iMau == 0) {
+			cout << "Mau so khong duoc bang 0. Vui long nhap lai!\n";
+		}
+	}
+	RutGon();
 }
 
 void PhanSo::Xuat() {
-    cout << iTu << "/" << iMau;
+	if (iTu == 0) {
+		cout << "0";
+	}
+	else if (iMau == 1) {
+		cout << iTu;
+	}
+	else {
+		cout << iTu << "/" << iMau;
+	}
 }
 
 void PhanSo::RutGon() {
-    int ucln = UCLN(abs(iTu), abs(iMau));
-    iTu /= ucln;
-    iMau /= ucln;
+	if (iTu == 0) {
+		iMau = 1;
+		return;
+	}
+	int ucln = UCLN(abs(iTu), abs(iMau));
+	iTu /= ucln;
+	iMau /= ucln;
 
-    // Đưa dấu về tử
-    if (iMau < 0) {
-        iTu = -iTu;
-        iMau = -iMau;
-    }
+
+	if (iMau < 0) {
+		iTu = -iTu;
+		iMau = -iMau;
+	}
 }
 
-PhanSo PhanSo::Tong(PhanSo ps) {
-    PhanSo kq;
-    kq.iTu = iTu * ps.iMau + ps.iTu * iMau;
-    kq.iMau = iMau * ps.iMau;
-    kq.RutGon();
-    return kq;
+PhanSo PhanSo::TinhTong(PhanSo ps) {
+	PhanSo kq;
+	kq.iTu = iTu * ps.iMau + ps.iTu * iMau;
+	kq.iMau = iMau * ps.iMau;
+	kq.RutGon();
+	return kq;
 }
 
-PhanSo PhanSo::Hieu(PhanSo ps) {
-    PhanSo kq;
-    kq.iTu = iTu * ps.iMau - ps.iTu * iMau;
-    kq.iMau = iMau * ps.iMau;
-    kq.RutGon();
-    return kq;
+PhanSo PhanSo::TinhHieu(PhanSo ps) {
+	PhanSo kq;
+	kq.iTu = iTu * ps.iMau - ps.iTu * iMau;
+	kq.iMau = iMau * ps.iMau;
+	kq.RutGon();
+	return kq;
 }
 
-PhanSo PhanSo::Tich(PhanSo ps) {
-    PhanSo kq;
-    kq.iTu = iTu * ps.iTu;
-    kq.iMau = iMau * ps.iMau;
-    kq.RutGon();
-    return kq;
+PhanSo PhanSo::TinhTich(PhanSo ps) {
+	PhanSo kq;
+	kq.iTu = iTu * ps.iTu;
+	kq.iMau = iMau * ps.iMau;
+	kq.RutGon();
+	return kq;
 }
 
-PhanSo PhanSo::Thuong(PhanSo ps) {
-    PhanSo kq;
-    kq.iTu = iTu * ps.iMau;
-    kq.iMau = iMau * ps.iTu;
-    kq.RutGon();
-    return kq;
+PhanSo PhanSo::TinhThuong(PhanSo ps) {
+	PhanSo kq;
+	if (ps.iTu == 0) {
+		cout << "Khong the chia cho 0!\n";
+		return kq;
+	}
+	kq.iTu = iTu * ps.iMau;
+	kq.iMau = iMau * ps.iTu;
+	kq.RutGon();
+	return kq;
 }
 
 int PhanSo::SoSanh(PhanSo ps) {
-    int left = iTu * ps.iMau;
-    int right = ps.iTu * iMau;
+	int a = iTu * ps.iMau;
+	int b = ps.iTu * iMau;
 
-    if (left > right) return 1;
-    if (left == right) return 0;
-    return -1;
+	if (a > b) return 1;
+	if (a == b) return 0;
+	return -1;
 }
