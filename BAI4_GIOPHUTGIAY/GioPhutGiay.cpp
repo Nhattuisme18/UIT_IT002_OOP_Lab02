9c77313d2ccd6e6b2102e18e96db1542c85d0e00
Nhattuisme18 <25521995@gm.uit.edu.vn>
2026-04-03 20:29:02 +0700

    Update GioPhutGiay.cpp

--- BAI4_GIOPHUTGIAY/GioPhutGiay.cpp
+++ BAI4_GIOPHUTGIAY/GioPhutGiay.cpp
@@ -1,34 +1,4 @@
-#include "GioPhutGiay.h"
-#include <iostream>
-
-using namespace std;
-
-
-
-void GioPhutGiay::Nhap() {
-	while (1) {
-		bool valid = 1;
-		cout << "Nhap gio, phut, giay: ";
-		cin >> iGio >> iPhut >> iGiay;
-		if (cin.fail()) {
-			cin.clear();
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-			cout << "Nhap sai dinh dang.\n";
-			continue;
-		}
-
-		if (iGio >= 0 && iGio < 24 && iPhut >= 0 && iPhut < 60 && iGiay >= 0 && iGiay < 60) break;
-		else {
-			if (iGio < 0 || iGio > 24) {
-				cout << "Gio khong hop le.\n";
-				valid = 0;
-			}
-			if (iPhut < 0 || iPhut>60) {
-				cout << "Phut khong hop le.\n";
-				valid = 0;
-			}
-			if (iGiay < 0 || iGiay >60) {
-				cout << "Giay khong hop le.\n";
+cout << "Giay khong hop le.\n";
 				valid = 0;
 			}
 		}
