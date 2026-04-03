9a8ebefa80bc66b8a5eea61bba489c76634ca467
Nhattuisme18 <25521995@gm.uit.edu.vn>
2026-04-03 17:05:19 +0700

    Update NgayThangNam.h

--- BAI1_NGAYTHANGNAM/NgayThangNam.h
+++ BAI1_NGAYTHANGNAM/NgayThangNam.h
@@ -1,16 +1,18 @@
 #ifndef NGAYTHANGNAM_H
 #define NGAYTHANGNAM_H
 
-class NgayThangNam {
-private:
-    int iNgay, iThang, iNam;
-    bool LaNamNhuan(int n);
-    int SoNgayTrongThang(int t, int n);
 
-public:
+class NgayThangNam
+{
+    public:
     void Nhap();
     void Xuat();
     void NgayThangNamTiepTheo();
+
+    private:
+        int iNgay, iThang, iNam;
+        bool kiemtraNamNhuan(int n);
+        int SoNgayTrongThang(int t, int n);
 };
 
-#endif
+#endif 
