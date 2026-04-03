6e7c9a16f12ad3566ae80fb20a53fb1483ac0b16
Nhattuisme18 <25521995@gm.uit.edu.vn>
2026-04-03 09:06:37 +0700

    Update NgayThangNam.cpp

--- BAI1_NGAYTHANGNAM/NgayThangNam.cpp
+++ BAI1_NGAYTHANGNAM/NgayThangNam.cpp
@@ -1,78 +1,58 @@
-#ifndef NGAYTHANGNAM_H
-#define NGAYTHANGNAM_H
-
-class NgayThangNam {
-private:
-    int iNgay, iThang, iNam;
-    bool LaNamNhuan(int n);
-    int SoNgayTrongThang(int t, int n);
-
-public:
-    void Nhap();
-    void Xuat();
-    void NgayThangNamTiepTheo();
-};
-
-#endif
-
-Code của file NgayThangNam.cpp :
 #include "NgayThangNam.h"
 #include <iostream>
-
 using namespace std;
 
-bool NgayThangNam::LaNamNhuan(int n) {
-    return (n % 4 == 0 && n % 100 != 0 || n % 400 == 0);
+
+bool NgayThangNam::kiemtraNamNhuan(int n)
+{
+	return (n % 4 == 0 && n % 100 != 0 || n % 400 == 0);
 }
 
-int NgayThangNam::SoNgayTrongThang(int t, int n) {
-    switch (t) {
-    case 1:
-    case 3:
-    case 5:
-    case 7:
-    case 8:
-    case 10:
-    case 12:
-        return 31;
-    case 2:
-        if (LaNamNhuan(n)) {
-            return 29;
-        }
-        else return 28;
-    default: return 30;
-    }
-}
+int NgayThangNam::SoNgayTrongThang(int t, int n)
+{
+	int days[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+	if (kiemtraNamNhuan(n) && t == 2)
+		return 29;
+	return days[t];
 
+}
 void NgayThangNam::Nhap() {
 
-    do {
-        cout << "Nhap ngay: "; cin >> iNgay;
-        cout << "Nhap thang: "; cin >> iThang;
-        cout << "Nhap nam: "; cin >> iNam;
-        if (iThang < 1 || iThang > 12 || iNgay < 1 || iNgay > SoNgayTrongThang(iThang, iNam) || iNam < 0) {
-            cout << "Ngay khong hop le, vui long nhap lai!\n";
-        }
-    } while (iThang < 1 || iThang > 12 || iNgay < 1 || iNgay > SoNgayTrongThang(iThang, iNam) || iNam < 0);
-}
+	while (1) {
+		bool valid = 1;
+		cout << "Nhap ngay thang nam: ";
+		cin >> iNgay >> iThang >> iNam;
+		if (iNgay < 1 || iNgay > SoNgayTrongThang(iThang, iNam)) {
+			valid = 0;
+			cout << "Ngay khong hop le.\n";
+		}
 
+		if (iThang < 1 || iThang > 12) {
+			valid = 0;
+			cout << "Thang khong hop le.\n";
+		}
+
+		if (iNam < 0) {
+			valid = 0;
+			cout << "Nam khong hop le.\n";
+		}
+		if (valid)break;
+	}
+}
 void NgayThangNam::NgayThangNamTiepTheo() {
-    iNgay++;
-    if (iNgay > SoNgayTrongThang(iThang, iNam)) {
-        iNgay = 1;
-        iThang++;
-        if (iThang > 12) {
-            iThang = 1;
-            iNam++;
-        }
-    }
+	iNgay++;
+	if (iNgay > SoNgayTrongThang(iThang, iNam)) {
+		iNgay = 1;
+		iThang++;
+		if (iThang > 12) {
+			iThang = 1;
+			iNam++;
+		}
+	}
 }
 
 void NgayThangNam::Xuat() {
-    cout << iNgay << "/";
-    cout << iThang << "/" << iNam << endl;
+	cout << iNgay << "/";
+	cout << iThang << "/" << iNam << endl;
 
 }
-
-
-
